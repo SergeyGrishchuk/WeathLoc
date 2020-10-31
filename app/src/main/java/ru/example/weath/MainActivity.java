790c344fe9bf31ff56688f298e30a2a20e3c4646
Sergey <sirgeyaid666@gmail.com>
2020-11-01 00:50:01 +0300

    HW#1 Перестал запускаться, не могу понять, где ошибка

--- app/src/main/java/ru/example/weath/MainActivity.java
+++ app/src/main/java/ru/example/weath/MainActivity.java
@@ -1,8 +1,10 @@
-package ru.example.weathloc;
+package ru.example.weath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ru.example.weathloc.R;
 
 public class MainActivity extends AppCompatActivity {
 
